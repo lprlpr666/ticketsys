f8e8f6d23118d5d7dbfc8f6f3bf961b6bfa4e59b
whoiam <119673245+lprlpr666@users.noreply.github.com>
2023-06-02 16:38:15 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "train.h"
+#include "user.h"
+#include "Tokenscanner.h"
+#include "bpt.h"
+#include "exceptions.h"
+#include "vector.h"
+#include "seatt.h"
+#include "orderfinish.h"
+#include "ticket.h"
+#include "time.h"
+#include "instructions.h"
+#include "map.h"
+int main() {
+    start();
+    return 0;
+}
+
